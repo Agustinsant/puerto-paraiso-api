8776950ad7e724b67024ba06bff3861a45525446
AgustinSant <agustinmsant@gmail.com>
2022-01-02 16:34:34 -0300

    solving altlas issues

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const dbUser = process.env.dbUser
 const dbPass = process.env.dbPass
 
-const url =`mongodb+srv://${dbUser}:${dbPass}@cluster0.u6op0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const url =`mongodb+srv://${dbUser}:${dbPass}@cluster0.u6op0.mongodb.net/puertoParaiso?retryWrites=true&w=majority`
 
 
 mongoose.connect(url, {
